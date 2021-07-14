62cbea3fdbf232cef8b685e3e88093bebf59c8cd
Kerrymon <krollwtf@gmail.com>
2021-07-14 17:00:05 +0500

    Added sending files in telegram
    patch 0.10

--- EyesHealth.py
+++ EyesHealth.py
@@ -1,48 +1,73 @@
-import multiprocessing
+import sys
+import numpy as np
+import cv2 as cv
 import tkinter as tk
-import cv2
+from tkinter import ttk
+from tkinter import *
+import requests
+import telegram
 
-e = multiprocessing.Event()
 
-
-def startrecording(e):
-    cap = cv2.VideoCapture(0)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc,  20.0, (640,480))
-
-    while(cap.isOpened()):
-        if e.is_set():
-            cap.release()
-            out.release()
-            cv2.destroyAllWindows()
-            e.clear()
+def cam():
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
         ret, frame = cap.read()
-        if ret==True:
-            out.write(frame)
-        else:
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        color = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
+        cv.imshow('frame', color)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv.destroyAllWindows()
             break
 
-def start_recording_proc():
-    global p
-    p = multiprocessing.Process(target=startrecording, args=(e,))
-    p.start()
+    cap.release()
+    cv.destroyAllWindows()
 
 
-def stoprecording():
-    e.set()
-    p.join()
+def saving_video():
+    cap = cv.VideoCapture(0)
+    # Define the codec and create VideoWriter object
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        frame = cv.flip(frame, 0)
+        # write the flipped frame
+        out.write(frame)
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
 
-    root.quit()
-    root.destroy()
+def sendandclose():
+    bot = telegram.Bot('1800511746:AAFW0CUXSEaiFsxSUgYhWqOQ8fwTJeehpnU')
 
-#def output():
+    bot.send_video(chat_id='-1001523285912', video=open('output.avi', 'rb'), timeout=1000000)
+
+    sys.exit()
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("%dx%d+0+0" % (800, 600))
-    startbutton=tk.Button(root,width=10,height=1,text='START',command=start_recording_proc)
-    stopbutton=tk.Button(root,width=10,height=1,text='STOP', command=stoprecording)
-    startbutton.pack()
-    stopbutton.pack()
+
+
+    root = Tk()
+    root.title("EyesHealth")
+    root.geometry("150x110")
+    root.resizable(width=False,height=False)
+    start_button = tk.Button(root, text="Start", command=saving_video, height=3, width=20).grid(column=0, row=0)
+    stop_button = tk.Button(root, text="Send and close", command=sendandclose, height=3, width=20).grid(column=0, row=1)
+
     root.mainloop()
+
+
+
